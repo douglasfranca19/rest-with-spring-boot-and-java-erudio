f935d9988d2a847c6647207edfc01d8ee00ee6ec
douglasfranca19 <francadouglas19@gmail.com>
2024-01-07 13:03:23 -0300

    Classes separadas nos pacotes

--- src/main/java/br/com/erudio/controllers/MathController.java
+++ src/main/java/br/com/erudio/controllers/MathController.java
@@ -1,4 +1,4 @@
-package br.com.erudio;
+package br.com.erudio.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,70 +7,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.converter.NumberConverter;
 import br.com.erudio.exceptions.UnsupportedMathOperationExceptions;
+import br.com.erudio.math.SimpleMath;
 
 @RestController
 public class MathController {
 
 	private final AtomicLong counter = new AtomicLong();
 
+	private SimpleMath math = new SimpleMath();
+
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwO}", method = RequestMethod.GET)
 	public Double sum(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwO") String numberTwO) throws Exception {
 
-		if (!isNumeric(numberOne) || !isNumeric(numberTwO)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwO)) {
 			throw new UnsupportedMathOperationExceptions("Please set a numeric value");
 		}
 
-		return convertDouble(numberOne) + convertDouble(numberTwO);
+		return math.sum(NumberConverter.convertDouble(numberOne), NumberConverter.convertDouble(numberTwO));
 	}
 
 	@RequestMapping(value = "/sub/{numberOne}/{numberTwO}", method = RequestMethod.GET)
 	public Double sub(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwO") String numberTwO) throws Exception {
 
-		if (!isNumeric(numberOne) || !isNumeric(numberTwO)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwO)) {
 			throw new UnsupportedMathOperationExceptions("Please set a numeric value");
 		}
 
-		return convertDouble(numberOne) - convertDouble(numberTwO);
+		return math.sub(NumberConverter.convertDouble(numberOne), NumberConverter.convertDouble(numberTwO));
 	}
-	
+
 	@RequestMapping(value = "/mul/{numberOne}/{numberTwO}", method = RequestMethod.GET)
 	public Double mul(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwO") String numberTwO) throws Exception {
 
-		if (!isNumeric(numberOne) || !isNumeric(numberTwO)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwO)) {
 			throw new UnsupportedMathOperationExceptions("Please set a numeric value");
 		}
 
-		return convertDouble(numberOne) * convertDouble(numberTwO);
+		return math.mul(NumberConverter.convertDouble(numberOne), NumberConverter.convertDouble(numberTwO));
 	}
-	
+
 	@RequestMapping(value = "/div/{numberOne}/{numberTwO}", method = RequestMethod.GET)
 	public Double div(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwO") String numberTwO) throws Exception {
 
-		if (!isNumeric(numberOne) || !isNumeric(numberTwO)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwO)) {
 			throw new UnsupportedMathOperationExceptions("Please set a numeric value");
 		}
 
-		return convertDouble(numberOne) / convertDouble(numberTwO);
+		return math.div(NumberConverter.convertDouble(numberOne), NumberConverter.convertDouble(numberTwO));
 	}
 
-	private Double convertDouble(String strNumber) {
-		if (strNumber == null)
-			return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if (isNumeric(number))
-			return Double.parseDouble(number);
-		return 0D;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if (strNumber == null)
-			return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
 }
---
+++ src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,25 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne, Double numberTwO) {
+
+		return numberOne + numberTwO;
+	}
+
+	public Double sub(Double numberOne, Double numberTwO) {
+
+		return numberOne - numberTwO;
+	}
+
+	public Double mul(Double numberOne, Double numberTwO) {
+
+		return numberOne * numberTwO;
+	}
+
+	public Double div(Double numberOne, Double numberTwO) {
+
+		return numberOne / numberTwO;
+	}
+
+}

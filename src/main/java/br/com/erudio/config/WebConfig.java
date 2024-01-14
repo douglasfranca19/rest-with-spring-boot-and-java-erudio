8093b0b1ac63cd156beb879d4ba999d6b3276074
douglasfranca19 <francadouglas19@gmail.com>
2024-01-13 22:07:48 -0300

    Seção 13 finalizada (meu código não está igual ao do professor, consumes e producers de xml e yaml não estão funcionando legal)

---
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,35 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		/*//via queryparam.  http://localhost:8080/person?mediaType=xml
+		configurer.favorParameter(true)
+		.parameterName("mediaType").ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);*/
+			
+		//via headerparam.  http://localhost:8080/api/person/v1
+				configurer.favorParameter(false)
+				.ignoreAcceptHeader(false)
+				.useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON)
+					.mediaType("json", MediaType.APPLICATION_JSON)
+					.mediaType("xml", MediaType.APPLICATION_XML)
+					.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+				
+	}
+	
+}
